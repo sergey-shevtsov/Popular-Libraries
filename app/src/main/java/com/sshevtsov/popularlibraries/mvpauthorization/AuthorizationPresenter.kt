a90085c5682d68cd0290822139f59e4986e69182
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-10 17:19:50 +0300

    Добавил в Presenter фрагмента авторизации валидацию введенных данных и переход на фрагмент приветствия

--- app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationPresenter.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationPresenter.kt
@@ -1,7 +1,26 @@
 package com.sshevtsov.popularlibraries.mvpauthorization
 
 import com.github.terrakok.cicerone.Router
+import com.sshevtsov.popularlibraries.mvpgreetings.GreetingsScreen
 import moxy.MvpPresenter
 
 class AuthorizationPresenter(private val router: Router) : MvpPresenter<AuthorizationView>() {
+
+    fun onSignInButtonClicked(login: String, password: String) {
+        var validationPassed = true
+
+        if (password.isBlank()) {
+            viewState.showEmptyPasswordError()
+            validationPassed = false
+        }
+
+        if (login.isBlank()) {
+            viewState.showEmptyLoginError()
+            validationPassed = false
+        }
+
+        if (validationPassed) router.navigateTo(GreetingsScreen(login))
+    }
+
+
 }
