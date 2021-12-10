13d13b10e69764aa5813222d9a54f286007d9096
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-10 17:19:14 +0300

    Добавил во фрагмент авторизации выводить ошибки, если поля пустые

--- app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationFragment.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationFragment.kt
@@ -2,6 +2,7 @@ package com.sshevtsov.popularlibraries.mvpauthorization
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import com.sshevtsov.popularlibraries.App
 import com.sshevtsov.popularlibraries.R
 import com.sshevtsov.popularlibraries.databinding.FragmentAuthorizationBinding
@@ -18,6 +19,35 @@ class AuthorizationFragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAuthorizationBinding.bind(view)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.signInButton.setOnClickListener {
+            presenter.onSignInButtonClicked(
+                binding.loginEditText.text.toString(),
+                binding.passwordEditText.text.toString()
+            )
+        }
+
+        binding.loginEditText.doOnTextChanged { _, _, _, _ ->
+            binding.loginInputLayout.error = null
+        }
+
+        binding.passwordEditText.doOnTextChanged { _, _, _, _ ->
+            binding.passwordInputLayout.error = null
+        }
+    }
+
+    override fun showEmptyLoginError() {
+        binding.loginInputLayout.error = getString(R.string.empty_field_error_text)
+        binding.loginEditText.requestFocus()
+    }
+
+    override fun showEmptyPasswordError() {
+        binding.passwordInputLayout.error = getString(R.string.empty_field_error_text)
+        binding.passwordEditText.requestFocus()
     }
 
     companion object {
--- app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationView.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationView.kt
@@ -3,7 +3,14 @@ package com.sshevtsov.popularlibraries.mvpauthorization
 import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndSingleStrategy
 import moxy.viewstate.strategy.StateStrategyType
+import moxy.viewstate.strategy.alias.OneExecution
+import moxy.viewstate.strategy.alias.SingleState
 
-@StateStrategyType(AddToEndSingleStrategy::class)
 interface AuthorizationView : MvpView {
+
+    @OneExecution
+    fun showEmptyLoginError()
+
+    @OneExecution
+    fun showEmptyPasswordError()
 }
