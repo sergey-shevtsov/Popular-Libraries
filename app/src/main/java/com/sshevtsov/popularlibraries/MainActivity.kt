aef78e2169decf9a40e177e7e4216366761da649
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-10 15:01:45 +0300

    Добавил в MainActivity контейнер для фрагментов и binding

--- app/src/main/java/com/sshevtsov/popularlibraries/MainActivity.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/MainActivity.kt
@@ -2,11 +2,14 @@ package com.sshevtsov.popularlibraries
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sshevtsov.popularlibraries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
