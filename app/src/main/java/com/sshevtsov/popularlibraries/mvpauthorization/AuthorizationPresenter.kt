c267eda7e8a088ad06ad1cddf0db9f41a322b96f
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-11 18:16:04 +0300

    Добавил валидацию полей по указанным в enum классе Regex. Перенес все управление view в презентер.

--- app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationPresenter.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpauthorization/AuthorizationPresenter.kt
@@ -16,8 +16,11 @@ class AuthorizationPresenter(
 
         if (!validateData(login, password)) return
 
-        viewState.setState(ViewState.LOADING)
-        viewState.clearFocus()
+        viewState.apply {
+            setState(ViewState.LOADING)
+            hideKeyboard()
+            clearFocus()
+        }
 
         userRepository.getUserByLoginAndPassword(login, password)
             .observeOn(AndroidSchedulers.mainThread())
@@ -31,23 +34,59 @@ class AuthorizationPresenter(
 
     private fun validateData(login: String, password: String): Boolean {
 
-        var isValid = true
-
-        if (password.isBlank()) {
-            viewState.showEmptyPasswordError()
-            isValid = false
-        }
-
-        if (login.isBlank()) {
-            viewState.showEmptyLoginError()
-            isValid = false
-        }
+        var isValid = validateField(password, AuthorizationFieldEnum.PASSWORD)
+        isValid = validateField(login, AuthorizationFieldEnum.LOGIN) && isValid
 
         return isValid
     }
 
+    private fun validateField(text: String, fieldEnum: AuthorizationFieldEnum): Boolean {
+
+        var errorType = ERROR_NONE
+
+        val isValid = if (text.isBlank()) {
+            errorType = ERROR_EMPTY
+            false
+        } else if (!text.matches(fieldEnum.regex)) {
+            errorType = ERROR_NO_VALID
+            false
+        } else true
+
+        doActionOnError(errorType, fieldEnum)
+
+        return isValid
+    }
+
+    private fun doActionOnError(errorType: Int, fieldEnum: AuthorizationFieldEnum) {
+
+        when (errorType) {
+            ERROR_NONE -> {
+                return
+            }
+            ERROR_EMPTY -> {
+                viewState.showEmptyFieldError(fieldEnum)
+            }
+            ERROR_NO_VALID -> {
+                viewState.showNoValidFieldError(fieldEnum)
+            }
+        }
+
+        viewState.requestFocusOnField(fieldEnum)
+        viewState.showKeyboard()
+    }
+
+    fun onFieldTextChanged(fieldEnum: AuthorizationFieldEnum) {
+        viewState.cleanFieldError(fieldEnum)
+    }
+
     fun onEmptyAreaClicked() {
+        viewState.hideKeyboard()
         viewState.clearFocus()
     }
 
+    companion object {
+        private const val ERROR_NONE = 0
+        private const val ERROR_EMPTY = 1
+        private const val ERROR_NO_VALID = 2
+    }
 }
