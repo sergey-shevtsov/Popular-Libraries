d02e9ec7f65122dabdacbd9eab8f2f4f8dcf05ca
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-14 22:48:19 +0300

    В SquareNumberFragment изменил родительский класс на InteractiveMvpAppCompatFragment. Добавил реакцию на дополнительные пользовательские события.

--- app/src/main/java/com/sshevtsov/popularlibraries/mvpsquarenumber/SquareNumberFragment.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpsquarenumber/SquareNumberFragment.kt
@@ -2,13 +2,14 @@ package com.sshevtsov.popularlibraries.mvpsquarenumber
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import com.sshevtsov.popularlibraries.R
 import com.sshevtsov.popularlibraries.databinding.FragmentSquareNumberBinding
-import moxy.MvpAppCompatFragment
+import com.sshevtsov.popularlibraries.mvpbase.InteractiveMvpAppCompatFragment
 import moxy.ktx.moxyPresenter
 
 class SquareNumberFragment
-    : MvpAppCompatFragment(R.layout.fragment_square_number), SquareNumberView {
+    : InteractiveMvpAppCompatFragment(R.layout.fragment_square_number), SquareNumberView {
 
     private val presenter by moxyPresenter { SquareNumberPresenter() }
 
@@ -19,13 +20,38 @@ class SquareNumberFragment
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentSquareNumberBinding.bind(view)
 
+        initView()
+    }
+
+    private fun initView() {
+
         binding.countButton.setOnClickListener {
             presenter.onCountButtonClicked(binding.numberInputEditText.text.toString())
+        }
+
+        binding.numberInputEditText.doOnTextChanged { _, _, _, _ ->
+            presenter.onFieldTextChanged()
+        }
+
+        binding.root.setOnClickListener {
+            presenter.onEmptyAreaClicked()
         }
     }
 
     override fun setResult(result: String) {
         binding.resultTextView.text = result
+    }
+
+    override fun showEmptyFieldError() {
+        binding.numberInputLayout.error = getString(R.string.empty_field_error_text)
+    }
+
+    override fun cleanFieldError() {
+        binding.numberInputLayout.error = null
+    }
+
+    override fun requestFocusOnField() {
+        binding.numberInputEditText.requestFocus()
     }
 
     override fun onDestroyView() {
