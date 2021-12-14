5b519db6b63f7cdb032061391463188d7314193d
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-14 21:23:10 +0300

    Добавил сохранение подписки в CompositeDisposable и удаление при onDestroy презентера.

--- app/src/main/java/com/sshevtsov/popularlibraries/mvpsquarenumber/SquareNumberPresenter.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpsquarenumber/SquareNumberPresenter.kt
@@ -1,11 +1,14 @@
 package com.sshevtsov.popularlibraries.mvpsquarenumber
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import moxy.MvpPresenter
 
 class SquareNumberPresenter : MvpPresenter<SquareNumberView>() {
+
+    private val compositeDisposable = CompositeDisposable()
 
     private val subject = BehaviorSubject.create<String>()
 
@@ -19,16 +22,23 @@ class SquareNumberPresenter : MvpPresenter<SquareNumberView>() {
     }
 
     private fun observeChanges() {
-        subject
+        val disposable = subject
             .map { it.toDouble() }
             .map { it * it }
             .map { removeTail(it) }
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { viewState.setResult(it) }
+
+        compositeDisposable.add(disposable)
     }
 
     private fun removeTail(number: Double): String =
         number.toString().replace(".0", "")
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
+    }
 
 }
