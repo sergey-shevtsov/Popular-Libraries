423dbb05076436d5f520e1482238eab6f4223aa7
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-14 23:25:21 +0300

    Вернул установку ContentView в MainActivity через метод onCreate.

--- app/src/main/java/com/sshevtsov/popularlibraries/mvpmain/MainActivity.kt
+++ app/src/main/java/com/sshevtsov/popularlibraries/mvpmain/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.sshevtsov.popularlibraries.mvpmain
 
+import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.sshevtsov.popularlibraries.App
 import com.sshevtsov.popularlibraries.R
+import com.sshevtsov.popularlibraries.databinding.ActivityMainBinding
 import com.sshevtsov.popularlibraries.navigation.CustomNavigator
 import com.sshevtsov.popularlibraries.util.hideKeyboard
 import moxy.MvpAppCompatActivity
 import moxy.ktx.moxyPresenter
 
-class MainActivity : MvpAppCompatActivity(R.layout.activity_main), MainView {
+class MainActivity : MvpAppCompatActivity(), MainView {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     private val presenter by moxyPresenter { MainPresenter(App.instance.router) }
 
     private val navigator = CustomNavigator(this, R.id.fragment_container)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_app_bar_menu, menu)
